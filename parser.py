f5a3d57c756c7c1af39351f8469116cef235e496
Salokhiddin Rakhmonkulov <infinity10k@ya.ru>
2020-07-21 19:08:34 +0700

    update

--- parser.py
+++ parser.py
@@ -1,7 +1,12 @@
 import json
 import requests
 
-apiKey = 'APIKEY'
+con_file = open('config.json')
+config = json.load(con_file)
+con_file.close()
+
+apiKey = config['apiKey']
+searchURL = 'https://api.elsevier.com/content/search/scopus?'
 fields = 'title,citedby-count,publicationName,creator,doi,affilname,affiliation-country,affiliation-city,affiliation-сountry,afid,authid,authname,authkeywords'
 #fields = 'publicationName'
 
@@ -11,17 +16,18 @@ with open('files/source_id.csv', 'r') as f:
 length = len(F)
 
 for sourceID, i in zip(F, range(length)):
-    query = 'SOURCE-ID(' + str(sourceID) + ') AND ( LIMIT-TO( DOCTYPE, "ar") OR LIMIT-TO( DOCTYPE, "cp" ) OR LIMIT-TO( DOCTYPE, "re") OR LIMIT-TO( DOCTYPE, "ch") )' # ISSN(20294913)
+    query = 'SOURCE-ID(' + str(sourceID) + ') AND ( LIMIT-TO( DOCTYPE, "ar") OR LIMIT-TO( DOCTYPE, "cp" ) OR LIMIT-TO( DOCTYPE, "re") OR LIMIT-TO( DOCTYPE, "ch") )' # ISSN()
 
-    url = 'https://api.elsevier.com/content/search/scopus?start=0&count=1&apiKey='+apiKey+'&query='+query+'&field=publicationName&subjarea=ECON&date=2011-2020'
+    url = searchURL+'start=0&count=1&apiKey='+apiKey+'&query='+query+'&field=publicationName&subjarea=ECON&date=2011-2020'
 
     response = requests.get(url)
     output = json.loads(response.text)
     totalResults = int(output['search-results']['opensearch:totalResults'])
+    #itemsPerPage = int(output['search-results']['opensearch:itemsPerPage'])
 
     if totalResults != 0:
         for startPage in range(0, totalResults, 25):
-            url = 'https://api.elsevier.com/content/search/scopus?start='+str(startPage)+'&apiKey='+apiKey+'&query='+query+'&subjarea=ECON&field='+ fields +'&date=2011-2020&view=COMPLETE'
+            url = searchURL+'start='+str(startPage)+'&count=25&apiKey='+apiKey+'&query='+query+'&subjarea=ECON&field='+fields+'&date=2011-2020&view=COMPLETE'
             response = requests.get(url)
 
             if response.status_code == 200:
