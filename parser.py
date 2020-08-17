701daba14e8471f478589840b0613b8c18236ecc
Salokhiddin Rakhmonkulov <33337801+Infinity10K@users.noreply.github.com>
2020-08-17 18:51:09 +0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@
 import json
 import requests
 import psycopg2
-from re import sub
+from re import sub, split
 
 # functions (a separate module will be created in the future)
 
@@ -24,16 +24,16 @@ def get_type(table, data):
 
             result = cur.fetchall()
 
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        return result
 
-    return result
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error, 'in function get_type')
 
 
 def get_source(table, data):
     sql_select = 'select id from ' + table + ' where id = %s;'
     sql_insert = 'insert into ' + table + \
-        ' (id, issn, name, type_id) values(%s, %s, %s, %s);'
+        ' (id, name, type_id) values(%s, %s, %s);'
 
     try:
         cur.execute(sql_select, (data[0], ))
@@ -47,10 +47,10 @@ def get_source(table, data):
 
             result = cur.fetchall()
 
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        return result
 
-    return result
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error, 'in function get_source')
 
 # opening config file
 
@@ -63,7 +63,7 @@ con_file.close()
 
 apiKey = config['apiKey']
 searchURL = 'https://api.elsevier.com/content/search/scopus?'
-fields = 'title,citedby-count,publicationName,creator,doi,affilname,affiliation-country,affiliation-city,affiliation-сountry,afid,authid,authname,authkeywords,source-id,coverDate,description,subtypeDescription,aggregationType,issn,identifier'
+fields = 'title,citedby-count,publicationName,creator,doi,affilname,affiliation-country,affiliation-city,affiliation-сountry,afid,authid,authname,authkeywords,source-id,coverDate,description,subtypeDescription,aggregationType,identifier'
 
 # opening file with sources id
 
@@ -71,6 +71,7 @@ with open('files/source_id.csv', 'r') as f:
     F = [line.strip() for line in f]
 
 length = len(F)
+sum_results, sum_errors = 0, 0
 
 # db connection
 
@@ -92,109 +93,147 @@ with conn:
                 ') AND ( LIMIT-TO( DOCTYPE, "ar") OR LIMIT-TO( DOCTYPE, "cp" ) OR LIMIT-TO( DOCTYPE, "re") OR LIMIT-TO( DOCTYPE, "ch") )'
 
             url = searchURL+'start=0&count=1&apiKey='+apiKey+'&query=' + \
-                query+'&field=publicationName&subjarea=ECON&date=2011-2020'
+                query+'&subjarea=ECON&date=2011-2021'
 
             # getting the initial result
 
             response = requests.get(url)
-            output = json.loads(response.text)
-            totalResults = int(output['search-results']
-                               ['opensearch:totalResults'])
 
-            if totalResults != 0:  # if number of results not zero
+            if response.status_code == 200:
 
-                # we parse all pages
+                output = json.loads(response.text)
+                totalResults = int(output['search-results']
+                                   ['opensearch:totalResults'])
 
-                for startPage in range(0, totalResults, 25):
-                    url = searchURL+'start='+str(startPage)+'&count=25&apiKey='+apiKey+'&query=' + \
-                        query+'&subjarea=ECON&field='+fields+'&date=2011-2020&view=COMPLETE'
-                    response = requests.get(url)
+                if totalResults != 0:  # if number of results not zero
 
-                    output = json.loads(response.text)
-                    entries = output['search-results']['entry']
+                    # we parse all pages
 
-                    for entry in entries:
-                        try:
-                            # add/get doc_type and source_type
+                    for startPage in range(0, totalResults, 25):
+                        url = searchURL+'start='+str(startPage)+'&count=25&apiKey='+apiKey+'&query=' + \
+                            query+'&subjarea=ECON&field='+fields+'&date=2011-2021&view=COMPLETE'
 
-                            doc_type_id = get_type(
-                                'doc_types', entry['subtypeDescription'])[0][0]
-                            source_type_id = get_type(
-                                'source_types', entry['prism:aggregationType'])[0][0]
+                        response = requests.get(url)
 
-                            # add/get source
+                        if response.status_code == 200:
 
-                            source_id = get_source(
-                                'sources', (entry['source-id'], entry['prism:issn'], entry['prism:publicationName'], source_type_id))
+                            output = json.loads(response.text)
+                            entries = output['search-results']['entry']
 
-                            #
+                            for entry in entries:
+                                try:
+                                    try:
+                                            subtypeDescription = entry['subtypeDescription']
+                                            aggregationType = entry['prism:aggregationType']
+                                            source_id = entry['source-id']
+                                            publicationName = entry['prism:publicationName']
+                                            identifier = entry['dc:identifier']
+                                            affiliations = entry['affiliation']
+                                            creator = entry['dc:creator']
+                                    
+                                    except:
+                                            sum_errors += 1
+                                            continue
 
-                            affil = []
-                            article_id = sub(r'\D', '', entry['dc:identifier'])
+                                        # add/get doc_type and source_type
+                                    if subtypeDescription in 'Article, Conference Paper, Review, Book Chapter' and aggregationType in 'Journal, Conference Proceeding':
 
-                            # add affil in db and get array of affil_id
+                                        doc_type_id = get_type(
+                                            'doc_types', subtypeDescription)[0][0]
+                                        source_type_id = get_type(
+                                            'source_types', aggregationType)[0][0]
 
-                            for affiliation in entry['affiliation']:
-                                afid = affiliation['afid']
+                                        # add/get source
 
-                                affil.append(int(afid))
+                                        source_id = get_source(
+                                            'sources', (source_id, publicationName, source_type_id))[0][0]
 
-                                res = cur.execute(
-                                    'select id from affiliations where id = ' + afid + ';')
+                                        #
 
-                                if res:
-                                    cur.execute(
-                                        'update affiliations set articles = articles || ' + article_id + '::bigint WHERE id = ' + afid)
-                                else:
-                                    cur.execute('insert into affiliations (articles, city, country, id, name) values(array[%s]::bigint[], %s, %s, %s, %s) on conflict do nothing;',
-                                                (int(article_id), affiliation['affiliation-city'], affiliation['affiliation-country'], afid, affiliation['affilname']))
+                                        affil = []
+                                        article_id = sub(r'\D', '', identifier)
 
-                            # add/get authors
+                                        # add affil in db and get array of affil_id
 
-                            sub_authors = []
-                            creator = entry['dc:creator']
+                                        for affiliation in affiliations:
 
-                            for author in entry['author']:
+                                            afid = affiliation['afid']
 
-                                authid = author['authid']
+                                            affil.append(int(afid))
 
-                                if creator != author['authname']:
-                                    sub_authors.append(int(authid))
+                                            cur.execute('select id from affiliations where id = ' + afid + ';')
 
-                                auth_affil = []
+                                            res = cur.fetchall()
 
-                                for affil_ in author['afid']:
-                                    auth_affil.append(int(affil_['$']))
+                                            if res:
+                                                cur.execute(
+                                                    'update affiliations set articles = articles || ' + article_id + '::bigint WHERE id = ' + afid + ';')
+                                            else:
+                                                cur.execute('insert into affiliations (articles, city, country, id, name) values(array[%s]::bigint[], %s, %s, %s, %s);',
+                                                            (int(article_id), affiliation['affiliation-city'], affiliation['affiliation-country'], afid, affiliation['affilname']))
 
-                                auth = cur.execute(
-                                    'select id from authors where id = ' + authid + ';')
+                                        # add/get authors
 
-                                if auth:
-                                    cur.execute(
-                                        'update authors set affiliations = affiliations || ' + affil_ + '::bigint[] WHERE id = ' + authid)
-                                else:
-                                    cur.execute('insert into authors (affiliations, id, name) values(%s, %s, %s) on conflict do nothing;',
-                                                (affil_, authid, entry['authname']))
+                                        sub_authors = []
 
-                                cur.execute('update authors set articles = articles || ' +
-                                            article_id + '::bigint WHERE id = ' + authid)
+                                        for author in entry['author']:
 
-                            # add article in db
+                                            authid = author['authid']
 
-                            key_words = split(r'\W+', entry['authkeywords'])
+                                            authname = author['authname']
 
-                            cur.execute('insert into articles (affiliations, authkeywords, citedby_count, creator, description, doctype, doi, id, pub_date, source_id, subauthors, title) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) on conflict do nothing;',
-                                        (affil, key_words, entry['citedby-count'], creator, entry['dc:description'], doc_type_id, entry['prism:doi'],
-                                         article_id, entry['prism:coverDate'], source-id, sub_authors, entry['dc:title']))
+                                            if creator != authname:
+                                                sub_authors.append(int(authid))
+                                            else:
+                                                creator_id = int(authid)
 
-                        except:
-                            with open('errors.json', 'a') as f:
-                                json.dump(entry, f)
+                                            auth_affil = []
 
-            conn.commit()
+                                            for affil_ in author['afid']:
+                                                auth_affil.append(int(affil_['$']))
 
-            print(str(progress+1) + ' of ' + str(length) +
-                  ' done (' + str(round((progress+1)/length, 2)) + '%)')
+                                            cur.execute('select id from authors where id = ' + authid + ';')
+
+                                            auth = cur.fetchall()
+
+                                            if auth:
+                                                cur.execute('update authors set affiliations = affiliations || %s::bigint[] WHERE id = ' + authid + ';', (auth_affil, ))
+                                            else:
+                                                cur.execute('insert into authors (affiliations, id, name) values(%s, %s, %s);',
+                                                            (auth_affil, authid, authname))
+
+                                            cur.execute('update authors set articles = articles || ' +
+                                                        article_id + '::bigint WHERE id = ' + authid + ';')
+
+                                        # add article in db
+
+                                        try:
+                                            key_words = split(r'\W+\s', entry['authkeywords'])
+                                        except:
+                                            key_words = []
+
+                                        try:
+                                            description = entry['dc:description']
+                                        except:
+                                            description = ''
+
+                                        cur.execute('insert into articles (affiliations, authkeywords, citedby_count, creator, description, doc_type, id, pub_date, source_id, subauthors, title) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) on conflict do nothing;',
+                                                    (affil, key_words, entry['citedby-count'], creator_id, description, doc_type_id,  
+                                                    article_id, entry['prism:coverDate'], source_id, sub_authors, entry['dc:title']))
+                                        
+                                        conn.commit()
+                                except (Exception, psycopg2.DatabaseError) as error:
+                                    sum_errors += 1
+                                    print(error)
+                                
+
+                    
+
+                print(str(progress+1) + ' of ' + str(length) +
+                      ' done (' + str(round((progress+1)/length, 2)) + '%)')
+
+                sum_results += totalResults
+                print('Results:', sum_results, 'Errors:', sum_errors)
 
 
 conn.close()
